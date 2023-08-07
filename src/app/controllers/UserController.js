327d3febd4a96a5379e38f2017c5fa279dfbbcc1
Vicente <pvicenteneto7@gmail.com>
2023-08-07 08:43:10 -0300

    feat: criptografia na senha

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,15 +1,40 @@
-/* import User from '../models/User'
+import User from '../models/User'
 import { v4 } from 'uuid'
+import * as Yup from 'yup'
 
 
 class UserController{
-   async store(request, response){
-        const { name, email, password_hash, admin } = request.body
+  async store(request, response){
+
+   const schema = Yup.object().shape({
+    name: Yup.string().required(),
+    email: Yup.string().email().required(),
+    password: Yup.string().required().min(6),
+    admin: Yup.boolean(),
+   })
+
+   try {
+    await schema.validateSync(request.body, { abortEarly: false })
+   } catch(err) {
+    return response.status(400).json({error: err.errors})
+   }
+
+
+        const { name, email, password, admin } = request.body
+
+        const userExists = await User.findOne ({
+          where: { email },
+         })
+         
+         if (userExists) {
+          return response.status(400).json({error: 'User already exists'})
+         }
+
         const user = await User.create({
             id: v4(),
             name,
             email,
-            password_hash,
+            password,
             admin
           })
  
@@ -18,4 +43,4 @@ class UserController{
     }
 }
 
-export default new UserController() */
+export default new UserController()
