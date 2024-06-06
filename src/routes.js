a0992981e1eed73a368d916e9731e2e75927055d
Vicente <pvicenteneto7@gmail.com>
2024-06-06 12:00:56 -0300

    feat: criação e ajuste do category controller

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ import UserController from './app/controllers/UserController'
 import LoginController from './app/controllers/LoginController'
 import ProductController from './app/controllers/ProductController'
 import authMiddleware from './app/middlewares/auth'
+import CategoryController from './app/controllers/CategoryController'
 
 const upload = multer(multerConfig)
 const routes = new Router()
@@ -16,6 +17,10 @@ routes.use(authMiddleware) // as rotas abaixo serão chamadas pelo middleware
 
 routes.post('/products', upload.single('file'), ProductController.store)
 routes.get('/products', ProductController.index)
+
+routes.post("/categories", upload.single("file"), CategoryController.store)
+routes.get("/categories", CategoryController.index)
+routes.put("/categories/:id", upload.single("file"), CategoryController.update)
 
 export default routes
 
