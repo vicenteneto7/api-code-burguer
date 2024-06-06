3ba47becbc79e4bfbf6a0e51c1957d7644f1a438
Vicente <pvicenteneto7@gmail.com>
2024-06-06 12:08:25 -0300

    feat: relacionamento de tabelas

--- src/app/models/products.js
+++ src/app/models/products.js
@@ -1,26 +1,33 @@
 import Sequelize, { Model } from 'sequelize'
 
-
 class Product extends Model {
-    static init(sequelize) {
-        super.init({
-          name: Sequelize.STRING,
-          price: Sequelize.INTEGER,
-          category: Sequelize.STRING,
-          path: Sequelize.STRING,
-          url: {
-            type: Sequelize.VIRTUAL,
-            get() {
-              return `http://localhost:3002/product-file/${this.path}`
-            }
-          }
+  static init(sequelize) {
+    super.init(
+      {
+        name: Sequelize.STRING,
+        price: Sequelize.INTEGER,
+        category: Sequelize.STRING,
+        path: Sequelize.STRING,
+        url: {
+          type: Sequelize.VIRTUAL,
+          get() {
+            return `http://localhost:3002/product-file/${this.path}`
+          },
         },
-            {
-                sequelize,
-            }
+      },
+      {
+        sequelize,
+      },
+    )
+    return this
+  }
 
-        )
-    }
+  static associate(models) {
+    this.belongsTo(models.Category, {
+      foreignKey: "category_id",
+      as: "category",
+    })
+  }
 }
 
 export default Product
--- src/database/index.js
+++ src/database/index.js
@@ -2,19 +2,32 @@ import Sequelize from 'sequelize'
 import configDatabase from '../config/database'
 import User from '../app/models/User'
 import Product from '../app/models/Products'
+import mongoose from 'mongoose'
 
-const models = [ User, Product ]
+const models = [User, Product]
 
 class Database {
-    constructor() {
-        this.init()
-    }
+  constructor() {
+    this.init()
+    this.mongo()
+  }
 
-    init() {
-        this.connection = new Sequelize(configDatabase)
-        models.map((model) => model.init(this.connection))
-
-    }
+  init() {
+    this.connection = new Sequelize(configDatabase)
+    models
+      .map((model) => model.init(this.connection))
+      .map(
+        (model) => model.associate && model.associate(this.connection.models),
+      )
+  }
+  mongo() {
+    this.mongoConnection = mongoose.connect(
+      'mongodb://mongo:AaFCAg1A3466Dgh4fHbgFC4hc1bACg2C@roundhouse.proxy.rlwy.net:23464',
+      {
+        useNewUrlParser: true,
+      },
+    )
+  }
 }
 
 export default new Database()
